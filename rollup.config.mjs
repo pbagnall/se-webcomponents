df8fd0102f80644d2f140894efd50e1d77f42b5b
pete <pete@surfaceeffect.com>
2024-02-23 16:19:24 +0000

    Changes to build, to create multiple js files as output.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,19 +3,28 @@ import terser from "@rollup/plugin-terser";
 
 const production = !process.env.ROLLUP_WATCH;
 
-export default {
-    input: 'src/main.js',
-    output: {
-        sourcemap: true,
-        format: 'es',
-        name: 'app',
-        dir: 'public/scripts'
+export default [
+    {
+        input: 'src/main.js',
+        output: {
+            sourcemap: !production,
+            format: 'es',
+            name: 'app',
+            dir: 'public/scripts',
+            manualChunks: function(path) {
+                if (path.indexOf('/components/') !== -1) return 'components';
+                if (path.indexOf('/template/') !== -1) return 'template';
+                if (path.indexOf('/lib/') !== -1) return 'lib';
+            },
+            chunkFileNames: "[name].js"
+        },
+        plugins: [
+           resolve(),
+            // terser(),
+        ],
+        watch: { clearScreen: true },
+        rollupOutputOptions: {
+            entryFileNames: '[name].js',
+        }
     },
-    plugins: [
-        resolve(),
-        terser(),
-    ],
-    watch: {
-        clearScreen: true
-    }
-};
+];
